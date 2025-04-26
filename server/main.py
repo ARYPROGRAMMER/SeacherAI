1a7478f794a4402459cce9ac155834a1121e2168
Arya Pratap Singh <notaryasingh@gmail.com>
2025-04-26 16:30:26 +0530

    llm service creation + websocket ready
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- server/main.py
+++ server/main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI
 from pydantic_models.chat_body import ChatBody
 from services.search_service import SearchService
 from services.sort_source import SortSourceService
+from services.llm_Service import LLMService
 
 
 app = FastAPI()
 search_service = SearchService()
 sort_source_service = SortSourceService()
+llm_service = LLMService()
 
 @app.post("/chat")
 def chat_endpoint(body: ChatBody):
     search_results = search_service.web_search(body.query)
     sorted_results = sort_source_service.sort_service(body.query, search_results)
-    
+    response = llm_service.generate_response(body.query, sorted_results)
 
-    return body.query
+    return response
---
+++ server/services/llm_service.py
@@ -0,0 +1,29 @@
+import google.generativeai as genai
+from config import Settings
+
+settings = Settings()
+class LLMService:
+    def __init__(self):
+        genai.configure(api_key=settings.GEMINI_API_KEY)
+        self.model = genai.GenerativeModel("gemini-2.5-pro-preview-03-25") # gemini-2.5-flash-preview-04-17
+
+    def generate_response(self, query: str, search_results: list[dict]):
+        # engineer prompt
+
+        context = "\n\n".join([
+            f"Source {i+1} ({res["url"]}):\n{res["content"]}"
+            for i, res in enumerate(search_results)
+        ])
+
+        prompt = f"""
+        Context from web search:
+        {context}
+
+        Query: {query}      
+
+        Please provide a comprehensive, detailed, well-cited accurate response using the above context. Think and reason deeply. Ensure it answers the query the user is asking. Do not use your knowledge until it is absolutely necessary.
+        """
+
+        response = self.model.generate_content(prompt)
+
+        return response.text
